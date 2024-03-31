3553e20071b0da81b1c53aa4328278f5b04de083
Henrik Lia <henrik.lia@outlook.com>
2024-04-01 00:27:19 +0200

    Cleanup plugins

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -6,20 +6,16 @@ return {
     'nvim-lua/plenary.nvim',
     {
       'nvim-telescope/telescope-fzf-native.nvim',
-
       build = 'make',
-
       cond = function()
         return vim.fn.executable 'make' == 1
       end,
     },
     { 'nvim-telescope/telescope-ui-select.nvim' },
-
     { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
   },
   config = function()
     require('telescope').setup {
-
       extensions = {
         ['ui-select'] = {
           require('telescope.themes').get_dropdown(),

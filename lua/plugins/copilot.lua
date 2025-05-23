438d7be7954453a0e8e269b5be8ebb66755e04cf
Henrik Lia <henrik.lia@outlook.com>
2025-05-23 15:29:49 +0200

    Update

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,4 +1,7 @@
 return {
   'github/copilot.vim',
-  config = function() end,
+  config = function()
+    vim.keymap.set('i', '<C-K>', '<Plug>(copilot-accept-word)')
+    vim.keymap.set('i', '<C-O>', '<Plug>(copilot-accept-line)')
+  end,
 }

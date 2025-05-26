dbaafc89ada7be614d4d5dbb4f1fd613ee243afa
Henrik Lia <henrik.lia@outlook.com>
2025-05-26 12:39:47 +0200

    Squash merge try-migrate into master

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,217 +1,148 @@
 return {
-  'neovim/nvim-lspconfig',
-  dependencies = {
-    { 'williamboman/mason.nvim', version = '1.11.0' },
-    { 'williamboman/mason-lspconfig.nvim', version = '1.32.0' },
-    'WhoIsSethDaniel/mason-tool-installer.nvim',
-    { 'j-hui/fidget.nvim', opts = {} },
-    { 'folke/neodev.nvim', opts = {} },
-    { 'saghen/blink.cmp' },
+  {
+    'folke/lazydev.nvim',
+    ft = 'lua',
+    opts = {
+      library = {
+        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+      },
+    },
   },
-  version = 'v1.8.0',
-  config = function()
-    vim.api.nvim_create_autocmd('LspAttach', {
-      group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
-      callback = function(event)
-        local client = vim.lsp.get_client_by_id(event.data.client_id)
-        if client and client.server_capabilities.documentHighlightProvider then
-          vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-            buffer = event.buf,
-            callback = vim.lsp.buf.document_highlight,
-          })
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      { 'mason-org/mason.nvim', opts = {} },
+      'WhoIsSethDaniel/mason-tool-installer.nvim',
+      'mason-org/mason-lspconfig.nvim',
+      { 'j-hui/fidget.nvim', opts = {} },
+      'saghen/blink.cmp',
+    },
+    config = function()
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
+        callback = function(event)
+          local client = vim.lsp.get_client_by_id(event.data.client_id)
+          if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+            local highlight_augroup = vim.api.nvim_create_augroup('lsp-highlight', { clear = false })
+            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+              buffer = event.buf,
+              group = highlight_augroup,
+              callback = vim.lsp.buf.document_highlight,
+            })
 
-          vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-            buffer = event.buf,
-            callback = vim.lsp.buf.clear_references,
-          })
-        end
-      end,
-    })
+            vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+              buffer = event.buf,
+              group = highlight_augroup,
+              callback = vim.lsp.buf.clear_references,
+            })
 
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = require('blink.cmp').get_lsp_capabilities(capabilities)
-
-    local util = require 'lspconfig/util'
-
-    local function get_python_venv_path(workspace)
-      -- Use activated virtualenv.
-      if vim.env.VIRTUAL_ENV then
-        return vim.env.VIRTUAL_ENV .. '/bin/python'
-      end
-
-      -- Find and use virtualenv in workspace directory.
-      for _, pattern in ipairs { '*', '.*' } do
-        local match = vim.fn.glob(workspace .. '/' .. pattern .. '/pyvenv.cfg')
-        if match ~= '' then
-          return vim.fs.dirname(match) .. '/bin/python'
-        end
-      end
-
-      return nil
-    end
-
-    local servers = {
-      zls = {
-        filetypes = { 'zig' },
-        root_dir = util.root_pattern '.git',
-      },
-      -- bacon = {
-      --   filetypes = { 'rust' },
-      --   root_dir = util.root_pattern 'Cargo.toml',
-      -- },
-      -- ['bacon-ls'] = {
-      --   filetypes = { 'rust' },
-      --   root_dir = util.root_pattern 'Cargo.toml',
-      -- },
-      rust_analyzer = {
-        filetypes = { 'rust' },
-        root_dir = util.root_pattern 'Cargo.toml',
-        settings = {
-          ['rust-analyzer'] = {
-            checkOnSave = true,
-            check = {
-              command = 'clippy',
-            },
-            -- diagnostics = {
-            --   enable = false,
-            -- },
-            cargo = {
-              allFeatures = true,
-            },
-          },
-        },
-      },
-      taplo = {
-        filetypes = { 'toml' },
-      },
-      lua_ls = {
-        settings = {
-          Lua = {
-            completion = {
-              callSnippet = 'Replace',
-            },
-          },
-        },
-      },
-      pyright = {
-        autostart = false,
-      },
-      basedpyright = {
-        before_init = function(_, config)
-          local venv_path = get_python_venv_path(config.root_dir)
-          if venv_path then
-            config.settings.python.pythonPath = venv_path
+            vim.api.nvim_create_autocmd('LspDetach', {
+              group = vim.api.nvim_create_augroup('lsp-detach', { clear = true }),
+              callback = function(event2)
+                vim.lsp.buf.clear_references()
+                vim.api.nvim_clear_autocmds { group = 'lsp-highlight', buffer = event2.buf }
+              end,
+            })
           end
         end,
-        settings = {
-          python = {},
-          basedpyright = {
-            analysis = {
-              autoSearchPaths = true,
-              useLibraryCodeForTypes = true,
-              typeCheckingMode = 'basic',
-              autoImportCompletion = true,
+      })
+
+      local function get_python_venv_path(workspace)
+        if vim.env.VIRTUAL_ENV then
+          return vim.env.VIRTUAL_ENV .. '/bin/python'
+        end
+
+        if not workspace then
+          return nil
+        end
+
+        -- Find and use virtualenv in workspace directory.
+        for _, pattern in ipairs { '*', '.*' } do
+          local match = vim.fn.glob(workspace .. '/' .. pattern .. '/pyvenv.cfg')
+          if match ~= '' then
+            return vim.fs.dirname(match) .. '/bin/python'
+          end
+        end
+
+        return nil
+      end
+
+      local servers = {
+        rust_analyzer = {
+          settings = {
+            ['rust-analyzer'] = {
+              checkOnSave = true,
+              check = {
+                command = 'clippy',
+              },
+              cargo = {
+                allFeatures = true,
+              },
             },
           },
         },
-      },
-      ruff = {},
-      ['css-lsp'] = {
-        filetypes = { 'css', 'vue' },
-      },
-      volar = {
-        filetypes = { 'typescript', 'javascript', 'vue' },
-        root_dir = util.root_pattern 'src_*/App.vue',
-        init_options = {
-          vue = {
-            hybridMode = false,
-          },
-        },
-      },
-      eslint = {
-        filetypes = {
-          'javascript',
-          'typescript',
-          'vue',
-        },
-      },
-      prettierd = {
-        filetypes = { 'javascript', 'typescript', 'vue' },
-      },
-      ts_ls = {
-        autostart = false,
-        init_options = {
-          plugins = {
-            {
-              name = '@vue/typescript-plugin',
-              location = '/home/henrik/.local/share/pnpm/global/5/node_modules/@vue/typescript-plugin/',
-              languages = { 'javascript', 'typescript', 'vue' },
+        taplo = {},
+        lua_ls = {},
+        basedpyright = {
+          before_init = function(_, config)
+            local venv_path = get_python_venv_path(config.root_dir)
+            if venv_path then
+              config.settings.python.pythonPath = venv_path
+            end
+          end,
+          settings = {
+            python = {},
+            basedpyright = {
+              analysis = {
+                autoSearchPaths = true,
+                useLibraryCodeForTypes = true,
+                typeCheckingMode = 'basic',
+                autoImportCompletion = true,
+              },
             },
           },
         },
-        filetypes = {
-          'javascript',
-          'typescript',
-          'vue',
-        },
-        commands = {
-          OrganizeImports = {
-            function()
-              local params = {
-                command = '_typescript.organizeImports',
-                arguments = { vim.api.nvim_buf_get_name(0) },
-                title = '',
-              }
-              vim.lsp.buf.execute_command(params)
-            end,
-            description = 'Organize Imports',
+        ruff = {},
+        vue_ls = {
+          filetypes = { 'javascript', 'typescript', 'vue' },
+          init_options = {
+            vue = { hybridMode = false },
           },
         },
-      },
-      clangd = {},
-      -- shellcheck = {
-      --   filetypes = { 'sh', 'bash', 'zsh' },
-      -- },
+        eslint = { filetypes = { 'javascript', 'typescript', 'vue' } },
+        clangd = {},
+        bashls = {},
+      }
 
-      bashls = {
-        settings = {
-          filetypes = { 'sh', 'zsh' },
-        },
-      },
-    }
+      vim.api.nvim_create_augroup('AutoFormat', {})
 
-    vim.api.nvim_create_augroup('AutoFormat', {})
+      vim.api.nvim_create_autocmd('BufWritePost', {
+        pattern = '*.js,*.ts,*.vue',
+        group = 'AutoFormat',
+        command = 'silent! EslintFixAll',
+      })
 
-    vim.api.nvim_create_autocmd('BufWritePost', {
-      pattern = '*.js,*.ts,*.vue',
-      group = 'AutoFormat',
-      command = 'silent! EslintFixAll',
-    })
+      local ensure_installed_formatters = {
+        'stylua',
+        'shfmt',
+        'prettierd',
+      }
 
-    require('mason').setup()
+      require('mason-tool-installer').setup {
+        ensure_installed = ensure_installed_formatters,
+      }
 
-    local ensure_installed = vim.tbl_keys(servers or {})
-    vim.list_extend(ensure_installed, {
-      'stylua',
-      'shfmt',
-    })
-    require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+      local ensure_installed_lsps = vim.tbl_keys(servers or {})
 
-    require('mason-lspconfig').setup {
-      handlers = {
-        function(server_name)
-          local server = servers[server_name] or {}
+      require('mason-lspconfig').setup {
+        ensure_installed = ensure_installed_lsps,
+        automatic_enable = false,
+      }
 
-          server.capabilities = require('blink.cmp').get_lsp_capabilities(server.capabilities or {})
-          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-
-          require('lspconfig')[server_name].setup(server)
-        end,
-      },
-    }
-
-    -- TODO: Find out why this is needed
-    require('lspconfig').ruff.setup {}
-    -- require('lspconfig').bacon_ls.setup {}
-  end,
+      for name, server in pairs(servers) do
+        vim.lsp.config(name, server)
+        vim.lsp.enable(name, server.autostart == nil or server.autostart)
+      end
+    end,
+  },
 }
